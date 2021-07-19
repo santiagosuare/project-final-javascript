d7c044456960732ef78c5622f36d98121952b8ea
Santiago Suarez <santiagosuarez@Air-de-Santiago.fibertel.com.ar>
2021-07-19 20:05:11 -0300

    Ajax and Fix

--- js/scriptBuy.js
+++ js/scriptBuy.js
@@ -4,86 +4,87 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
+const FormasDePago = document.querySelector('#result');
 //BACKIN INFO JSON
 const libroJSON = sessionStorage.getItem('LibroBuy');
-// console.log(libroJSON);
 const libroObj = JSON.parse(libroJSON);
-// console.log(libroObj);
-
 let totalshow = '';
 
 function addLibroCompra(){
-
     librosMostrar = document.querySelector('.listaLibros');
-    // console.log(librosMostrar);
   
-
-    
     for(const libros of libroObj){
-
-        let textShow2 = `<li class="collection-item"> Libro: ` + libros.nombre + ` - Precio: $` + libros.precio + `</li>`
-        // librosMostrar.innerHTML = textShow2;
-        totalshow = totalshow + textShow2
-        // console.log(textShow2);
-        // console.log(totalshow);
+        let textShow2 = `<li class="collection-item"> Libro: ` + libros.nombre + ` - Precio: $` + libros.precio + `</li>`;
+        totalshow = totalshow + textShow2;
+  
         addTextHtml(totalshow);
     }
 
     function addTextHtml(totalshow){
-        // console.log(totalshow);
         librosMostrar.innerHTML = totalshow;
     }
-    
 }
 
 function addPrecioCompra(){
-
     librosPrecioMostrarConIVA = document.querySelector('.TotalConIVA');
     librosPrecioMostrarSinIVA = document.querySelector('.TotalSinIVA');
 
-    console.log(librosPrecioMostrarConIVA);
-    console.log(librosPrecioMostrarSinIVA);
-
-
     librosPrecioMostrarConIVA.innerHTML = '<li class="collection-header"><h4>Precio Total Con IVA: $ ' + sessionStorage.getItem('TotalPriceConIva'); + '</h4></li>';
     librosPrecioMostrarSinIVA.innerHTML = '<li class="collection-header"><h4>Precio Total Sin IVA: $ ' + sessionStorage.getItem('TotalPriceSinIva'); + '</h4></li>';
-
 }
-
-
 
 addLibroCompra();
 addPrecioCompra();
-
-
 
 $('form').on('submit', function(event) {
             
     event.preventDefault();
     
     let $inputName = $(this).find('#first_name');
-    console.log($inputName);
     let inputName = $inputName.val();
-    console.log(inputName);
 
     let $inputApellido = $(this).find('#last_name');
-    console.log($inputApellido);
     let inputApellido = $inputApellido.val();
-    console.log(inputApellido);
 
     let $inputTel = $(this).find('#phone');
-    console.log($inputTel);
     let inputTel = $inputTel.val();
-    console.log(inputTel);
 
     let $inputEmail = $(this).find('#email');
-    console.log($inputEmail);
     let inputEmail = $inputEmail.val();
-    console.log(inputEmail);
-
 
     $('#text-outputFactura').text("Factura a Nombre: " + inputName + " " + inputApellido);
     $('#text-outputFactura2').text("Telefono: " + inputTel);
-    $('#text-outputFactura3').text("Email: " + inputEmail);
+    $('#text-outputFactura3').text("Email: " + inputEmail + "          PENDIENTE A ACTUALIZACION");
 });
+
+$.ajax({
+    url:"/MOCK_DATA.json",
+    method: "GET",
+    dataType: "JSON",
+    success: function(data){
+       renderFormasPago(data);
+    },
+    error: function(jqHXR, status,error) {
+        console.log(jqHXR);
+        console.log(status);
+        console.log(error);
+    }
+})
+
+function renderFormasPago(data) {
+    FormasDePago.innerHTML = '';
+    
+    data.forEach(data=>{
+        const {id, Tarjeta, Cuotas} = data;
+
+        $("#cards").append(
+            `
+                <tr>
+                    <td>${id}</td>
+                    <td>${Tarjeta}</td>
+                    <td>${Cuotas}</td>
+                </tr>
+            `
+        );
+    });
+}
